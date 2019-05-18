69cad45898317f6a88615c003af5617253a435a1
Abhishek Patil <AbhishekPatil@gatech.edu>
2019-05-18 12:16:12 -0400

    server

---
+++ Express.js
@@ -0,0 +1,74 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Star Wars Characters (DATA)
+// =============================================================
+var tables = [
+  {
+    id: "1",
+    name: "Danerys",
+    email: "Danerys@hbo.com",
+    number: 1000000000
+  },
+];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  // res.send("Current Reservations and Waiting List")
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+// Displays all tables
+app.get("/tables", function(req, res) {
+  return res.json(tables);
+});
+
+// Displays a single table, or returns false
+// app.get("/reserve/:table", function(req, res) {
+//   var chosen = req.params.table;
+
+//   console.log(chosen);
+
+//   for (var i = 0; i < tables.length; i++) {
+//     if (chosen === tables[i].routeName) {
+//       return res.json(tables[i]);
+//     }
+//   }
+
+//   return res.json(false);
+// });
+
+// Create New Tables- takes in JSON input
+app.post("/reserve", function(req, res) {
+  // req.body hosts is equal to the JSON post sent from the user
+  // This works because of our body parsing middleware
+  var newTable = req.body;
+
+  console.log(newTable);
+
+  // We then add the json the user sent to the table array
+  tables.push(newTable);
+
+  // We then display the JSON to the users
+  res.json(newTable);
+});
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
